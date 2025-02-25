ff12150aaee46fdb5dc312997e518397ed599de7
jinjianli <jin2001@jinjianlis-MacBook-Air-6.local>
2025-02-25 11:53:26 -0600

    Part1 npu 1.3

--- npu/conv_npu.py
+++ npu/conv_npu.py
@@ -123,7 +123,10 @@ def conv2d(X, W, bias):
         bias_indicator = 0
 
         #we do the row tile chunk by chunk: 
-        out_h_tile_size = 4
+
+        #####warning#####
+        out_h_tile_size = 1  #failed to realize a more felxible framework
+        #now only size = 1 can work
         out_h_tile_amount = (out_height+out_h_tile_size-1) // out_h_tile_size
 
         #here, because conv needs additional lines(the marginal), we need
@@ -164,8 +167,8 @@ def conv2d(X, W, bias):
 
 
                 for out_h_i in nl.affine_range(out_h_tile_size):
-                    if output_h_start+out_h_i>=out_height:
-                        break
+                    #if output_h_start+out_h_i>=out_height:
+                    #    break
                     Output_row = nl.zeros((c_out_pmax, 
                                           out_width), 
                                         dtype=nl.float32, buffer=nl.psum
@@ -206,8 +209,8 @@ def conv2d(X, W, bias):
                     c_out_tile_start = c_out_tile_i*c_out_pmax
                     c_out_tile_end = c_out_tile_start+c_out_pmax
                     row_ind = output_h_start+out_h_i
-                    if row_ind<out_height:
-                        nl.store(X_out[b_i,c_out_tile_start:c_out_tile_end,row_ind,:],
+                    #if row_ind<out_height:
+                    nl.store(X_out[b_i,c_out_tile_start:c_out_tile_end,row_ind,:],
                         Output_row
                         #,mask=(row_ind<out_height)
                         )
