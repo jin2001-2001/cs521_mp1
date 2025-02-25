c2ea7cccc628d4a92655bb28ff06ee0c624b777a
jinjianli <jin2001@jinjianlis-MacBook-Air-6.local>
2025-02-25 11:12:49 -0600

    Part1 npu 1.3

--- npu/conv_npu.py
+++ npu/conv_npu.py
@@ -202,12 +202,12 @@ def conv2d(X, W, bias):
                          
                     Output_row = nisa.tensor_scalar(Output_row, np.add, bias_vertical_vec)
                     #Output_tiles[:, out_h_i,:] = nl.copy(Output_row)
-                #now Ouput_tiles is calculated completely::
+                    #now Ouput_tiles is calculated completely::
                     c_out_tile_start = c_out_tile_i*c_out_pmax
                     c_out_tile_end = c_out_tile_start+c_out_pmax
                     nl.store(X_out[b_i,c_out_tile_start:c_out_tile_end,output_h_start+out_h_i],
                         Output_row
-                        #,mask=(output_h_start+out_h_i<out_height)
+                        ,mask=(output_h_start+out_h_i<out_height)
                         )
                       
     return X_out
